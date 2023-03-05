528c3f2dea38a791544c2e96869259fd1438559a
iLightThings <gitgub@ggray.info>
2023-03-05 10:00:04 -0500

    Started Parsing NTLMSSP

--- encodeutil/encoding.go
+++ encodeutil/encoding.go
@@ -1,11 +1,26 @@
 package encodeutil
 
 import (
+	"encoding/binary"
+
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 )
 
+//https://gist.github.com/sail1972/c7f2da14d0f284f7d76a5afce7daacfc
+
 func EncodeToUTF16(s string) ([]byte, error) {
 	finalstring, _, err := transform.Bytes(unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewEncoder(), []byte(s))
 	return finalstring, err
+}
+
+//https://gist.github.com/sail1972/c7f2da14d0f284f7d76a5afce7daacfc
+
+func DecodeUTF16leBytes(s []byte) (string, error) {
+	bs_UTF8LE, _, err := transform.Bytes(unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewDecoder(), s)
+	return string(bs_UTF8LE), err
+}
+
+func BytesToUint16(s []byte) uint16 {
+	return binary.LittleEndian.Uint16(s)
 }
--- hashing/NTLM.go
+++ hashing/NTLM.go
@@ -1,8 +1,11 @@
 package hashing
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/md5"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -56,6 +59,8 @@ func GenNTOWFv2(user string, domain string, password string, ntlmhash []byte) ([
 
 }
 
+// ntProofStr
+// Hash or Password
 func GenerateNTLMv2ChallengeProof(challenge []byte, password string, ntlmhash []byte, username string, domain string, blob []byte) ([]byte, error) {
 
 	NTFOv2, err := GenNTOWFv2(username, domain, password, ntlmhash)
@@ -79,4 +84,33 @@ func genHMACMD5(key []byte, n []byte) ([]byte, error) {
 	hasher := hmac.New(md5.New, key)
 	_, err := hasher.Write(n)
 	return hasher.Sum(nil), err
+}
+
+type NTLMSSPObj struct {
+	NTMLSSPMessageType []byte //4 bytes, offset 8
+	TargetName         []byte // 8 bytes, offset 12
+
+}
+
+func ParseNTLMSSP(NTLMSSP []byte) (NTLMSSPObj, error) {
+	var NTSSPObj NTLMSSPObj
+
+	NTLMSSP_Identifyer := []byte{0x4e, 0x54, 0x4c, 0x4d, 0x53, 0x53, 0x50, 0x00}
+	if !bytes.Equal(NTLMSSP[0:8], NTLMSSP_Identifyer) {
+		return NTSSPObj, errors.New("NTLMSSP Header is not correct")
+
+	}
+
+	TargetNameHeader := NTLMSSP[12:20]
+	TargetNameLen := TargetNameHeader[0:2]
+
+	//Not needed
+	//TargetNameMaxLen := TargetNameHeader[2:4]
+	TargetNameBufferOffset := TargetNameHeader[4:8]
+	fmt.Printf("%+v\n", TargetNameBufferOffset)
+	fmt.Println(binary.LittleEndian.Uint16(TargetNameBufferOffset))
+	NTSSPObj.TargetName = NTLMSSP[binary.LittleEndian.Uint16(TargetNameBufferOffset) : binary.LittleEndian.Uint16(TargetNameBufferOffset)+binary.LittleEndian.Uint16(TargetNameLen)]
+
+	return NTSSPObj, nil
+
 }
